de21489fb188ead3dfc6c1b4c675b31403e71408
Sabella-8 <gsabella549@gmail.com>
2024-02-27 19:12:58 +0000

    level 3

--- challenge-high-score-tables/high-score/src/Data.jsx
+++ challenge-high-score-tables/high-score/src/Data.jsx
@@ -4,33 +4,38 @@ import Header from "./Header";
 import BodyTable from "./BodyTable";
 
 function Data() {
-  const scores = allCountryScores
-    .slice()
-    .sort((a, b) => a.name.localeCompare(b.name));
+  const scores = allCountryScores;
+
   return (
     <div className="big-table">
       <h1>High Score Per Country</h1>
 
-      {scores.map((score) => {
-        return (
-          <div key={score.name} className="small-table">
-            <Header name={score.name} />
-            <table key={score.name}>
-              <tbody>
-                {" "}
-                {score.scores.map((sco) => {
-                  return (
-                    <>
-                      {" "}
-                      <BodyTable n={sco.n} s={sco.s} />
-                    </>
-                  );
-                })}{" "}
-              </tbody>
-            </table>
-          </div>
-        );
-      })}
+      {scores
+        .slice()
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map((score) => {
+          return (
+            <div key={score.name} className="small-table">
+              <Header name={score.name} />
+              <table key={score.name}>
+                <tbody>
+                  {" "}
+                  {score.scores
+                    .slice()
+                    .sort((a, b) => b.s - a.s)
+                    .map((sco) => {
+                      return (
+                        <>
+                          {" "}
+                          <BodyTable n={sco.n} s={sco.s} />
+                        </>
+                      );
+                    })}{" "}
+                </tbody>
+              </table>
+            </div>
+          );
+        })}
     </div>
   );
 }
