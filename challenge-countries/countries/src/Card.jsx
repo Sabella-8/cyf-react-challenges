9e32f890273affa431fceb617b545354f9aa4d9f
Sabella-8 <gsabella549@gmail.com>
2024-03-27 01:01:40 +0000

    country-level 1

---
+++ challenge-countries/countries/src/Card.jsx
@@ -0,0 +1,10 @@
+function Card({ data }) {
+  return (
+    <div className="card">
+      <img src="https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg?auto=compress&cs=tinysrgb&w=600" />
+      <h1>{data.name}</h1>
+      <h2>{data.capital}</h2>
+    </div>
+  );
+}
+export default Card;
